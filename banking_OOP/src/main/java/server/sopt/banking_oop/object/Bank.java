419bb8ed09a370f78dac7721a8eae2a33bd8409a
rlarlgnszx <rlarlgnszx@naver.com>
2024-04-05 15:00:24 +0900

    withdraw , banktranfer & test person add

--- banking_OOP/src/main/java/server/sopt/banking_oop/object/Bank.java
+++ banking_OOP/src/main/java/server/sopt/banking_oop/object/Bank.java
@@ -6,28 +6,26 @@ import server.sopt.banking_oop.service.ServiceProduct;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class Bank extends ServiceProduct{
     //은행에 고객들,
-    private Set<Person> customers;
+    @Builder.Default
+    private Set<Person> customers =new HashSet<>();
     //은행이름?
-    private Map<Person,List<PersonPerBankJoiner>> db;
+    @Builder.Default
+    private Map<Person,List<PersonPerBankJoiner>> db = new HashMap<>();
 
     private String name;
     //이자율
     private Double interestRate;
     //적금율?
     private Double savingInterestRate ;
-
     @Override
 //    예금
     public void deposit(Person person,Double money) throws IOException {
@@ -37,29 +35,46 @@ public class Bank extends ServiceProduct{
         }
         PersonPerBankJoiner personPerBankJoiner = findPersonforAccount(person);
         personPerBankJoiner.setSaving(personPerBankJoiner.getSaving() + money);
+        person.setHaveMoney(person.getHaveMoney() - money);
     }
     @Override
-    public void savings(Person person,Double money) {
+    public void savings(Person person,Double money) throws IOException {
         registerPerson(person);
         if(checkMoney(person,money)){
             return;
         }
+        PersonPerBankJoiner personPerBankJoiner = findPersonforAccount(person);
+        personPerBankJoiner.setSaving(personPerBankJoiner.getSaving() + money);
+        person.setHaveMoney(person.getHaveMoney() - money);
     }
 // 출금
-    @Override
-    public void withDraw(Person person, Double money) throws IOException {
-        registerPerson(person);
-        PersonPerBankJoiner personPerBankJoiner = findPersonforAccount(person);
-        if(checkMoneyFromAccount(person,money,personPerBankJoiner)){
-            return;
-        }
+    public void withDraw(PersonPerBankJoiner personPerBankJoiner, Double money, Person person) throws IOException {
         personPerBankJoiner.setSaving(personPerBankJoiner.getSaving() - money);
-        return ;
+        person.setHaveMoney(person.getHaveMoney() + money);
     }
-    @Override
-    public void bankTransfer(Person person, Double money) throws IOException {
 
+    @Override
+    public void bankTransfer(PersonPerBankJoiner account, Double money,PersonPerBankJoiner account2) throws IOException {
+        if (isaccountHaveEnoughMoney(account.getSaving(), money)) return;
+        account.setSaving(account.getSaving() - money);
+        account2.setSaving(account2.getSaving() + money);
     }
+
+    private static boolean isaccountHaveEnoughMoney(Double account, Double money) {
+        if (account < money) {
+            System.out.println("Not Enough Money");
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void bankTransfer(Person person, Double money,PersonPerBankJoiner account2) throws IOException {
+        if (isaccountHaveEnoughMoney(person.getHaveMoney(), money)) return;
+        person.setHaveMoney(person.getHaveMoney()-money);
+        account2.setSaving(account2.getSaving() + money);
+    }
+
     private boolean checkMoneyFromAccount(Person person, Double money, PersonPerBankJoiner personPerBankJoiner) throws IOException {
         return personPerBankJoiner.getSaving() < person.getHaveMoney();
     }
@@ -67,14 +82,14 @@ public class Bank extends ServiceProduct{
     public void registePerson2Bank(Person person) {
         customers.add(person);
         PersonPerBankJoiner personPerBankJoiner = new PersonPerBankJoiner(person, this);
-        personPerBankJoiner.setSaving(.0);
+        personPerBankJoiner.setSaving(0.0);
         List<PersonPerBankJoiner> personPerBankJoiners = person.getBanks();
         personPerBankJoiners.add(personPerBankJoiner);
         person.setBanks(personPerBankJoiners);
         db.put(person, List.of(personPerBankJoiner));
     }
 
-        private boolean checkMoney(Person person, Double money) {
+    private boolean checkMoney(Person person, Double money) {
         if(person.getHaveMoney()< money) {
             System.out.println("Not enough Money....");
             return true;
@@ -96,7 +111,6 @@ public class Bank extends ServiceProduct{
     }
     private static boolean showAccount(List<PersonPerBankJoiner> personPerBankJoiners) {
         if(personPerBankJoiners.size()==1) {
-            System.out.println(personPerBankJoiners.get(0).toString());
             return true;
         }
         int i=0;
@@ -105,5 +119,13 @@ public class Bank extends ServiceProduct{
             System.out.println(i+" : "+ account.getAccount());i++;
         }
         return false;
+    }
+    @Override
+    public String toString() {
+        return
+                "Name :" + name + '\'' +
+                ", 이자율 = " + interestRate +
+                ", 적금율 = " + savingInterestRate
+                ;
     }
 }
--- banking_OOP/src/main/java/server/sopt/banking_oop/object/Person.java
+++ banking_OOP/src/main/java/server/sopt/banking_oop/object/Person.java
@@ -5,21 +5,32 @@ import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@ToString
+//@ToString
 @Getter
 @Setter
-@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
 public class Person {
     //bank, account 연결
     private String name;
-    private List<PersonPerBankJoiner> banks;
-    @NonNull
-    private Double haveMoney;
+    @Builder.Default
+    private List<PersonPerBankJoiner> banks = new ArrayList<>();
+//    @NonNull
+    private Double haveMoney ;
+    public void getPersonforBankInfo() {
+        int i =0;
+        for(PersonPerBankJoiner personPerBankJoiner : banks) {
+            System.out.println(i + ". " + personPerBankJoiner.getBank().getName() + " : Account:" + personPerBankJoiner.getAccount() + " , In Account Money : "+ personPerBankJoiner.getSaving());
+            i++;
+        }
+    }
+    public boolean ispersonHaveAccount() {
+        return !banks.isEmpty();
+
+    }
 }
