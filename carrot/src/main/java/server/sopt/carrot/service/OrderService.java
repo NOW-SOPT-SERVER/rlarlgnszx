e2dd45cd8de3a08b83ea767dc70edf698e7eec2e
rlarlgnszx <rlarlgnszx@naver.com>
2024-04-17 22:43:07 +0900

    [refactor] PUT시 후에 View에서 주문 조회를 위해 Body에서 PathVariable로 변경 구현  (#8)

--- carrot/src/main/java/server/sopt/carrot/service/OrderService.java
+++ carrot/src/main/java/server/sopt/carrot/service/OrderService.java
@@ -3,6 +3,5 @@ package server.sopt.carrot.service;
 import server.sopt.carrot.dto.OrderCreate;
 
 public interface OrderService {
-
-    OrderCreate.Response createOrder(OrderCreate.Request req);
+    OrderCreate.Response createOrder(OrderCreate.Request req,Long productId);
 }
--- carrot/src/main/java/server/sopt/carrot/service/OrderServiceImpl.java
+++ carrot/src/main/java/server/sopt/carrot/service/OrderServiceImpl.java
@@ -4,22 +4,25 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import server.sopt.carrot.dto.OrderCreate;
-import server.sopt.carrot.entity.Order;
+import server.sopt.carrot.entity.OrderProcessor;
 import server.sopt.carrot.repo.OrderRepository;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+    private final ProductService productService;
+
     @Override
     @Transactional
-    public OrderCreate.Response createOrder(OrderCreate.Request req) {
-        Order order = Order.builder()
-                .productId(req.getProductId())
+    public OrderCreate.Response createOrder(OrderCreate.Request req,Long productId) {
+        OrderProcessor orderProcessor = OrderProcessor.builder()
+                .productId(productId)
                 .customerWhoSellId(req.getCustomerWhoSellId())
                 .customerWhoBuyId(req.getCustomerWhoBuyId())
                 .build();
-        orderRepository.save(order);
-        return OrderCreate.Response.fromEntity(order);
+        productService.soldProduct(productId);
+        orderRepository.save(orderProcessor);
+        return OrderCreate.Response.fromEntity(orderProcessor);
     }
 }
