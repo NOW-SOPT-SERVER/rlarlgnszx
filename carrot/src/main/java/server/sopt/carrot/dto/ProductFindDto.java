7733da0fdc44a29c300f2996e4cf330c78f5b72c
rlarlgnszx <rlarlgnszx@naver.com>
2024-04-17 22:41:33 +0900

    [fix] 상품 조회시 description 안보이는 이슈 해결 (#8)

--- carrot/src/main/java/server/sopt/carrot/dto/ProductFindDto.java
+++ carrot/src/main/java/server/sopt/carrot/dto/ProductFindDto.java
@@ -4,9 +4,9 @@ import server.sopt.carrot.constant.CellingStatus;
 import server.sopt.carrot.entity.Product;
 
 public record ProductFindDto(
-        String itemName,int price , CellingStatus cellingStatus
+        String itemName,int price , String itemDescription , CellingStatus cellingStatus
 ) {
     public static ProductFindDto of(Product product) {
-        return new ProductFindDto(product.getItemName(), product.getPrice(), product.getCellingStatus());
+        return new ProductFindDto(product.getItemName(), product.getPrice(),product.getItemDescription(), product.getCellingStatus());
     }
 }
