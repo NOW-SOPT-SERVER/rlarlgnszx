6fe534f8105ff200b3e80434bd0c3f45c33832de
rlarlgnszx <rlarlgnszx@naver.com>
2024-04-17 22:38:25 +0900

    [fix]  DB의 CreateAt, ModifiedAt에 Null값 들어가는 아슈 해결(#8)

--- carrot/src/main/java/server/sopt/carrot/CarrotApplication.java
+++ carrot/src/main/java/server/sopt/carrot/CarrotApplication.java
@@ -2,10 +2,11 @@ package server.sopt.carrot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CarrotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CarrotApplication.class, args);
 	}
