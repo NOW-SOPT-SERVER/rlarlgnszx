eb83aab8cbb772c1b1e7935d377cd0dfadc3428f
rlarlgnszx <rlarlgnszx@naver.com>
2024-04-17 16:49:55 +0900

    [feat] 각 객체의 로직 처리를 위한 서비스 인터페이스 구현 (#8)

---
+++ carrot/src/main/java/server/sopt/carrot/service/OrderService.java
@@ -0,0 +1,8 @@
+package server.sopt.carrot.service;
+
+import server.sopt.carrot.dto.OrderCreate;
+
+public interface OrderService {
+
+    OrderCreate.Response createOrder(OrderCreate.Request req);
+}
