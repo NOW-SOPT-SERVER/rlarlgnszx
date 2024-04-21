368e4267cc820c1fbd78da34234135db0018ce7c
rlarlgnszx <rlarlgnszx@naver.com>
2024-04-21 13:09:32 +0900

    [feat] 객체 생성시간, 변경시간 구현 (#11)

---
+++ week2/src/main/java/server/sopt/week2/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package server.sopt.week2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
