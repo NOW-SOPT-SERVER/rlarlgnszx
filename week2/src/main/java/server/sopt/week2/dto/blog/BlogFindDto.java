07820b9b2ed80b9aa205a577c42710a0e5eec010
rlarlgnszx <rlarlgnszx@naver.com>
2024-04-30 00:43:57 +0900

    [feat] GET 사용시 dto 구현 - #10

---
+++ week2/src/main/java/server/sopt/week2/dto/blog/BlogFindDto.java
@@ -0,0 +1,11 @@
+package server.sopt.week2.dto.blog;
+
+import server.sopt.week2.domain.Blog;
+
+public record BlogFindDto(
+        String title, String description
+        ) {
+    public static BlogFindDto of(Blog blog){
+        return new BlogFindDto(blog.getTitle(),blog.getDescription());
+    }
+}
