f60058b275039ff92b7c1a341fdbffb4cde0df0a
rlarlgnszx <rlarlgnszx@naver.com>
2024-04-30 00:48:44 +0900

    [feat] Repo 구현과 Post 생성 시 Blog 연관 조회 구현 - #10

--- week2/src/main/java/server/sopt/week2/repo/BlogRepostiory.java
+++ week2/src/main/java/server/sopt/week2/repo/BlogRepostiory.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import server.sopt.week2.domain.Blog;
 import server.sopt.week2.domain.Member;
 
+import java.util.Optional;
+
 public interface BlogRepostiory  extends JpaRepository<Blog,Long> {
 }
--- week2/src/main/java/server/sopt/week2/repo/MemberRepository.java
+++ week2/src/main/java/server/sopt/week2/repo/MemberRepository.java
@@ -2,7 +2,6 @@ package server.sopt.week2.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.sopt.week2.domain.Member;
-import server.sopt.week2.dto.MemberCreateDto;
 
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
