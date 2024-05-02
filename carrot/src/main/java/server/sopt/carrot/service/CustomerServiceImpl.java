99c5c43cf008f630ffa1824673785853b0b75099
rlarlgnszx <rlarlgnszx@naver.com>
2024-05-02 23:23:51 +0900

    [feat] Mapper, API 추가 - #13

--- carrot/src/main/java/server/sopt/carrot/service/CustomerServiceImpl.java
+++ carrot/src/main/java/server/sopt/carrot/service/CustomerServiceImpl.java
@@ -3,20 +3,23 @@ package server.sopt.carrot.service;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import server.sopt.carrot.entity.Customer;
-import server.sopt.carrot.dto.CustomerCreate;
-import server.sopt.carrot.dto.CustomerFindDto;
-import server.sopt.carrot.dto.ProductFindDto;
+import server.sopt.carrot.dto.customer.CustomerCreate;
+import server.sopt.carrot.dto.customer.CustomerFindDto;
+import server.sopt.carrot.dto.product.ProductFindDto;
+import server.sopt.carrot.error.ErrorMessage;
+import server.sopt.carrot.exception.BusinessException;
 import server.sopt.carrot.repo.CustomerRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
     private final CustomerRepository customerRepository;
-    private final ProductService productService;
 
     @Override
     @Transactional
@@ -29,17 +32,24 @@ public class CustomerServiceImpl implements CustomerService {
         customerRepository.save(customer);
         return CustomerCreate.Response.fromEntity(customer);
     }
+
     @Override
-    public CustomerFindDto getCustomerById(Long customerId) {
-        return CustomerFindDto.of(findCutomer(customerId));
-    }
-    private Customer findCutomer(Long id) {
-        return customerRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("NO ID MATCH FOR CUSTOMER"));
+    public List<CustomerFindDto> getAllCustomer() {
+        return customerRepository.findAll()
+                .stream().map(CustomerFindDto::of)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public List<ProductFindDto> getCustomerProducts(Long customerId) {
-        return productService.getAllProductByCustomerId(customerId);
+    public Customer getCustomerById(Long customerId) {
+        return findCutomer(customerId);
     }
+    private Customer findCutomer(Long id) {
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new BusinessException(ErrorMessage.CUSTOMER_NOT_FOUND));
+    }
+//    @Override
+//    public List<ProductFindDto> getCustomerProducts(Long customerId) {
+//        return productService.getAllProductByCustomerId(customerId);
+//    }
 }
