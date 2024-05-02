e2673ec974ba92819cd16e18c193d90d76f41622
rlarlgnszx <rlarlgnszx@naver.com>
2024-05-02 23:12:21 +0900

    [feat] Product API 추가 구현 - #13

--- carrot/src/main/java/server/sopt/carrot/service/ProductServiceImpl.java
+++ carrot/src/main/java/server/sopt/carrot/service/ProductServiceImpl.java
@@ -6,10 +6,17 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import server.sopt.carrot.constant.CellingStatus;
-import server.sopt.carrot.dto.ProductEdit;
+import server.sopt.carrot.constant.Place;
+import server.sopt.carrot.dto.product.ProductEdit;
+import server.sopt.carrot.dto.product.ProductGoodUpdateDto;
+import server.sopt.carrot.entity.Customer;
 import server.sopt.carrot.entity.Product;
-import server.sopt.carrot.dto.ProductCreate;
-import server.sopt.carrot.dto.ProductFindDto;
+import server.sopt.carrot.dto.product.ProductCreate;
+import server.sopt.carrot.dto.product.ProductFindDto;
+import server.sopt.carrot.error.ErrorMessage;
+import server.sopt.carrot.exception.BusinessException;
+import server.sopt.carrot.exception.NotFoundPlaceException;
+import server.sopt.carrot.mapper.ProductMapper;
 import server.sopt.carrot.repo.ProductRepository;
 
 import java.util.List;
@@ -20,22 +27,34 @@ import java.util.stream.Collectors;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private final CustomerService customerService;
+    private final ProductMapper productMapper;
 
     @Override
+    public Product findProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(
+                        () -> new BusinessException(ErrorMessage.PRODUCT_NOT_FOUND)
+                );
+    }
+    @Override
     public ProductFindDto getProductById(Long id) {
-        return productRepository.getProductById(id);
+        return productMapper.toProductFindDto(productRepository.getProductById(id));
     }
     @Transactional
     @Override
     public ProductCreate.Response createProductwithCustomerId(ProductCreate.Request req)
     {
+        Customer customer = customerService.getCustomerById(req.getCustomerId());
         Product product = Product.builder()
                 .price(req.getPrice())
                 .cellingStatus(CellingStatus.NOT_SOLD)
                 .itemName(req.getItemName())
-                .customerId(req.getCustomerId())
+                .customer(customer)
                 .itemDescription(req.getItemDescription())
+                .place(req.getPlace())
                 .build();
+
         productRepository.save(product);
         return ProductCreate.Response.fromEntity(product);
     }
@@ -61,18 +80,36 @@ public class ProductServiceImpl implements ProductService {
             ProductEdit.@Valid Request req) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new EntityNotFoundException("Not Found Product"));
-        product.setPrice(req.getPrice());
-        product.setItemDescription(req.getItemDescription());
-        product.setItemName(req.getItemName());
+        product.setPrice(req.getPrice() !=null ? req.getPrice() : product.getPrice());
+        product.setItemName(req.getItemName() !=null ? req.getItemName() : product.getItemName());
+        product.setItemDescription(req.getItemDescription()!=null ? req.getItemDescription() : product.getItemDescription());
         return ProductFindDto.of(product);
     }
 
     @Override
     @Transactional
-    public ProductFindDto soldProduct(Long productId) {
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new EntityNotFoundException("Not Found Product"));
+    public ProductFindDto soldProduct(Product product) {
+//        Product product = productRepository.findById(productId)
+//                .orElseThrow(() -> new BusinessException(ErrorMessage.PRODUC)T_NOT_FOUND));
         product.setCellingStatus(CellingStatus.SOLD);
         return ProductFindDto.of(product);
+    }
+
+    @Override
+    @Transactional
+    public List<ProductFindDto> getProductbyPlace(Place place) throws NotFoundPlaceException {
+        return productRepository.findProductsByPlace(place).stream().map(ProductFindDto::of)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    @Override
+    public ProductFindDto productGoodUporDown(Long productId, ProductGoodUpdateDto productGoodUpdateDto) {
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new BusinessException(ErrorMessage.PRODUCT_NOT_FOUND)
+        );
+        int point = productGoodUpdateDto.UporDown() ? 1 : -1;
+        product.setGood(product.getGood() + point);
+        return productMapper.toProductFindDto(product);
     }
 }
